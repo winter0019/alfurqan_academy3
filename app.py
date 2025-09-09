258db80df23f05b3b44974168f05df98a80329ca
winter0019 <121707096+winter0019@users.noreply.github.com>
2025-09-09 17:19:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -271,4 +271,8 @@ def view_receipt(payment_id):
 # INIT
 # ---------------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    with app.app_context():
+        db.create_all()  # Ensures tables are created
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
+
